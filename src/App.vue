65c6b6a59d1072ec0016c9b97fb9afa51bd9dbd6
Juan Polanco <jpolanco@intellisys.com.do>
2017-07-19 18:01:07 -0400

    Adding tabs to separate book list from book form. Separating config file from main code.

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,70 @@
 <template>
   <div id="app" class="container">
     <div class="panel-body">
-      <!-- Vue Material table -->
-      <md-table v-once>
-          <md-table-header>
-              <md-table-row>
-                  <md-table-head>Title</md-table-head>
-                  <md-table-head>Author</md-table-head>
-              </md-table-row>
-          </md-table-header>
+      <!-- VueMaterial Tabs -->
+      <md-tabs md-fixed>
+        <md-tab id="addbook" md-label="Add Book">
+          <!-- Input secton title -->
+          <md-toolbar>
+              <h3 class="md-title">Add Books</h3>
+          </md-toolbar>
 
-          <md-table-body>
-              <md-table-row v-for="book in books" :key="book.title">
-                  <md-table-cell><a v-bind:href="book.url">{{book.title}}</a></md-table-cell>
-                  <md-table-cell>{{book.author}}</md-table-cell>
-              </md-table-row>
-          </md-table-body>
-      </md-table>
-      <!-- Vue Material table -->
+          <!-- Input fields -->
+          <form novalidate v-on:submit.prevent="addBook">
+              <md-input-container>
+                  <label for="bookTitle">Title</label>
+                  <md-input type="text" v-model="newBook.title"></md-input>
+              </md-input-container>
+              <md-input-container>
+                  <label for="bookAuthor">Author</label>
+                  <md-input type="text" v-model="newBook.author"></md-input>
+              </md-input-container>
+              <md-input-container>
+                  <label for="bookUrl">URL</label>
+                  <md-input type="text" v-model="newBook.url"></md-input>
+              </md-input-container>
+              <md-button type="submit" class="md-raised md-primary">Add Book</md-button>
+          </form>
+        </md-tab>
+        <md-tab id="booklist" md-label="Book List">
+          <!-- Vue Material table -->
+          <md-table v-once>
+              <md-table-header>
+                  <md-table-row>
+                      <md-table-head>Title</md-table-head>
+                      <md-table-head>Author</md-table-head>
+                  </md-table-row>
+              </md-table-header>
+
+              <md-table-body>
+                  <md-table-row v-for="book in books" :key="book.id">
+                      <md-table-cell><a v-bind:href="book.url">{{book.title}}</a></md-table-cell>
+                      <md-table-cell>{{book.author}}</md-table-cell>
+                  </md-table-row>
+              </md-table-body>
+          </md-table>
+          <!-- Vue Material table -->
+        </md-tab>
+      </md-tabs>
     </div>
-    <!-- Input secton title -->
-    <md-toolbar>
-        <h3 class="md-title">Add Books</h3>
-    </md-toolbar>
-
-    <!-- Input fields -->
-    <form novalidate v-on:submit.prevent="addBook">
-        <md-input-container>
-            <label>Title</label>
-            <md-input v-model="newBook.title"></md-input>
-        </md-input-container>
-        <md-input-container>
-            <label>Author</label>
-            <md-input v-model="newBook.author"></md-input>
-        </md-input-container>
-        <md-input-container>
-            <label>URL</label>
-            <md-input v-model="newBook.url"></md-input>
-        </md-input-container>
-        <md-button type="submit" class="md-raised md-primary">Add Book</md-button>
-    </form>
   </div>
 </template>
 
 <script>
 import FireBase from 'firebase'
 import toastr from 'toastr'
+import dbConfig from './dbconfig'
 
-let config = {
-  apiKey: 'AIzaSyA5Bn7sjvs29EqnjbztqSWRWd7Regrq5o0',
-  authDomain: 'vuejs-firebase-01-5ab50.firebaseapp.com',
-  databaseURL: 'https://vuejs-firebase-01-5ab50.firebaseio.com/',
-  storageBucket: 'vuejs-firebase-01-5ab50.appspot.com',
-  messagingSenderId: '346193167287',
-  projectId: 'vuejs-firebase-01-5ab50'
-}
+// let config = {
+//   apiKey: 'AIzaSyA5Bn7sjvs29EqnjbztqSWRWd7Regrq5o0',
+//   authDomain: 'vuejs-firebase-01-5ab50.firebaseapp.com',
+//   databaseURL: 'https://vuejs-firebase-01-5ab50.firebaseio.com/',
+//   storageBucket: 'vuejs-firebase-01-5ab50.appspot.com',
+//   messagingSenderId: '346193167287',
+//   projectId: 'vuejs-firebase-01-5ab50'
+// }
 
-let app = FireBase.initializeApp(config)
+let app = FireBase.initializeApp(dbConfig)
 let db = app.database()
 
 let booksRef = db.ref('books')
@@ -92,14 +100,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/dbconfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyA5Bn7sjvs29EqnjbztqSWRWd7Regrq5o0',
+  authDomain: 'vuejs-firebase-01-5ab50.firebaseapp.com',
+  databaseURL: 'https://vuejs-firebase-01-5ab50.firebaseio.com/',
+  storageBucket: 'vuejs-firebase-01-5ab50.appspot.com',
+  messagingSenderId: '346193167287',
+  projectId: 'vuejs-firebase-01-5ab50'
+}
